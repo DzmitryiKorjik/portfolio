52362d42085ef60694049ad483f9d3afc6d5fc06
Dzmitryi <88487685+DzmitryiKorjik@users.noreply.github.com>
2025-01-22 17:26:52 +0100

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
         // Nodemailer configuration
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com', // SMTP-server Gmail
-            port: 587, // 465 Port TLS
-            secure: false, // true
+            port: 465, // 465/587 Port TLS
+            secure: true, // true/false
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
