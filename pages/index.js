fb345f1849a5baf3ca8c6e7f5fcb5a5c8ea91894
Dzmitryi <dmardovitch@gmail.com>
2025-01-22 14:23:01 +0100

    API

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 // Sélectionne le formulaire avec l'ID "contact-form" et ajoute un gestionnaire d'événement "submit"
 document
-  .getElementById("contact-form")
-  .addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      message: e.target.message.value,
-    };
+    .getElementById('contact-form')
+    .addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const formData = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            message: e.target.message.value,
+        };
 
-    try {
-      const response = await fetch("/api/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+        try {
+            const response = await fetch('/api/contact', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
 
-      if (response.ok) {
-        alert("Message envoyé avec succès !");
-      } else {
-        const errorData = await response.json();
-        console.error(errorData);
-        alert("Erreur lors de l'envoi du message.");
-      }
-    } catch (error) {
-      console.error("Erreur réseau :", error);
-      alert("Erreur lors de l'envoi du message.");
-    }
-  });
+            console.log(response);
+
+            if (response.ok) {
+                alert('Message envoyé avec succès !');
+            } else {
+                const errorData = await response.json();
+                console.error(errorData);
+                alert("Erreur lors de l'envoi du message.");
+            }
+        } catch (error) {
+            console.error('Erreur réseau :', error);
+            alert("Erreur lors de l'envoi du message.");
+        }
+    });
