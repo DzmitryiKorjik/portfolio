ff79f7c582d103057819ccd5573fa253df010f21
DzmitryiKorjik <dmardovitch@gmail.com>
2025-03-13 09:44:05 +0100

    modifie lien

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// ...existing code...
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/:page', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', `${req.params.page}.html`));
 });
-
-// ...existing code...
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
